80c3392ab2a2f9403aa666ec7cb58c13fecaf1aa
hom3mad3 <micaelarava@gmail.com>
2015-08-19 23:26:46 +0200

    first commit

---
+++ nuts.rb
@@ -0,0 +1,25 @@
+Camping.goes :Nuts
+
+module Nuts::Controllers
+   class Index < R '/'
+       def get
+           @time = Time.now
+           render :sundial
+           end
+       end
+    end
+
+module Nuts::Views
+    def layout
+        html do
+            head do
+                title {"Nuts and GORP"}
+                end
+            body {self << yield}
+            end
+        end
+    
+    def sundial
+        p "The current time is: #{@time}"
+        end
+    end
